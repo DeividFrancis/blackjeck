cd7452b63d04430a5b6f00ae7d900525369e1b43
DanielMeidiote <d4n1b0yofficial@gmail.com>
2018-10-24 12:02:10 -0400

    Inserção da Animação de Carta

--- app/js/animation.js
+++ app/js/animation.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     coins_transaction(2000, "remoded");
+    card_transaction();
 });
 
 
@@ -13,4 +14,14 @@ function coins_transaction(value, type) {
   setTimeout(function(){
     span.remove();
   }, 3000);
+}
+
+function card_transaction() {
+  var card = $("<img src='../img/cards/red_back.png' alt=''>");
+  card.attr('id', 'card-transaction');
+  $('.deck').append(card);
+
+  setTimeout(function(){
+    card.remove();
+  }, 1000);
 }
